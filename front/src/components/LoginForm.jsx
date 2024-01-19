fbe88e1f961914e7b29655d09c27c3b3b302ab66
안준선 <joonsun1006@naver.com>
2024-01-19 17:43:56 +0900

    refactor: #1 refactor loginForm styling

--- front/src/components/LoginForm.jsx
+++ front/src/components/LoginForm.jsx
@@ -4,7 +4,7 @@ import AccountManage from "./login/AccountManage";
 import SnsManage from "./login/SnsManage";
 import Input from "./store/Input";
 import Card from "./store/Card";
-
+import ButtonWrapper from "./signup/ButtonWrapper";
 
 const LoginForm = () => {
   return (
@@ -12,9 +12,15 @@ const LoginForm = () => {
     <Card>
       <LoginHeader />
       <form action="#">
-        <Input htmlFor="loginEmail" type="email" id="loginEmail" placeholder="이메일 입력" width="500px" />
-        <Input htmlFor="loginPw" type="password" id="loginPw" placeholder="비밀번호 입력" width="500px" />
-        <Button>로그인</Button>
+        
+        <Input htmlFor="loginEmail" type="email" id="loginEmail" placeholder="이메일 입력" width="300px" />
+        <Input htmlFor="loginPw" type="password" id="loginPw" placeholder="비밀번호 입력" width="300px" />
+        <Button
+          width="80%"
+          borderRadius="40px"
+        >
+          로그인
+        </Button>
       </form>
       <AccountManage />
       <SnsManage />
--- front/src/components/login/LoginHeader.jsx
+++ front/src/components/login/LoginHeader.jsx
@@ -1,19 +1,16 @@
 import styled from "styled-components";
 
-const LoginBox = styled.h1`
-  text-align: center;
+const ImgBox = styled.div`
+  display: flex;
+  justify-content: center;
   padding-bottom: 80px;
-
-  font-family: "Lexend Deca", sans-serif;
-  font-optical-sizing: auto;
-  font-weight: 400;
-  font-style: normal;
-  letter-spacing: -1.2px;
 `;
 
 const LoginHeader = () => {
   return (
-    <LoginBox>Flowering</LoginBox>
+    <ImgBox>
+      <img src="../../../src/assets/Logo.svg" alt="" />
+    </ImgBox>
   )
 };
 
