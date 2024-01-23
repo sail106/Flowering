38b7c9005867d24c804b916c8368ad8cb5ffeea4
안준선 <joonsun1006@naver.com>
2024-01-23 17:43:49 +0900

    style: #6 create community page, edit centercontainer

--- front/src/components/LoginForm.jsx
+++ front/src/components/LoginForm.jsx
@@ -8,33 +8,22 @@ import CenterContainer from "./store/CenterContainer";
 
 const LoginForm = () => {
   return (
-
     <Card>
       <CenterContainer>
-        <LoginHeader />
-      </CenterContainer>
-
+      <LoginHeader />
       <form action="#">
-        <CenterContainer>
-          
-          <Input htmlFor="loginEmail" type="email" id="loginEmail" placeholder="이메일 입력" width="300px" />
-        </CenterContainer>
-        <CenterContainer>
-          <Input htmlFor="loginPw" type="password" id="loginPw" placeholder="비밀번호 입력" width="300px" />
-        </CenterContainer>
-        <CenterContainer>
-
+        <Input htmlFor="loginEmail" type="email" id="loginEmail" placeholder="이메일 입력" width="300px" />
+        <Input htmlFor="loginPw" type="password" id="loginPw" placeholder="비밀번호 입력" width="300px" />
         <Button
           width="70%"
-          borderRadius="40px"
           marginTop="20px"
         >
           로그인
         </Button>
-        </CenterContainer>
       </form>
       <AccountManage />
       <SnsManage />
+      </CenterContainer>
     </Card>
 
 
---
+++ front/src/components/community/CommunityHeader.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const MyHeader = styled.h2`
+  
+`;
+
+const CommunityHeader = () => {
+  return (
+    <MyHeader>VIDEO COMMUNITY</MyHeader>
+  );
+};
+
+export default CommunityHeader;
