4e8dc89961f5cd7a097a5c24df544d03103bb751
김형민 <kimtuti@naver.com>
2024-01-24 17:59:05 +0900

    feat: #6 Community error 생성

--- back/back/src/main/java/com/sail/back/community/exception/CommunityErrorCode.java
+++ back/back/src/main/java/com/sail/back/community/exception/CommunityErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CommunityErrorCode {
-    DEFAULT("F", HttpStatus.BAD_REQUEST);
+    DEFAULT("F", HttpStatus.BAD_REQUEST),
+    COMMUNITY_NOT_FOUND("존재하지 않는 커뮤니티 입니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus httpStatus;
