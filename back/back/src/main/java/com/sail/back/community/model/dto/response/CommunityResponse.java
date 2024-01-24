8f6f576e0ffc6d507818afbacf868de29354e0db
김형민 <kimtuti@naver.com>
2024-01-24 17:59:22 +0900

    feat: #6 Community List Response 생성

--- back/back/src/main/java/com/sail/back/community/model/dto/response/CommunityResponse.java
+++ back/back/src/main/java/com/sail/back/community/model/dto/response/CommunityResponse.java
@@ -3,6 +3,8 @@ package com.sail.back.community.model.dto.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.sail.back.community.model.dto.response.enums.CommunityRole;
+import com.sail.back.community.model.entity.enums.CommunityStatus;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -20,6 +22,8 @@ public class CommunityResponse {
     private String title;
     private String content;
     private String thumbnailImgUrl;
+    private String createrName;
     private LocalDateTime dDay;
-
+    private CommunityStatus status;
+    private CommunityRole role;
 }
