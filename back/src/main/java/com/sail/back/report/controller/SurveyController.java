98451bccc3bf0c6ab7d78a9da625bea839f7d248
김형민 <kimtuti@naver.com>
2024-02-01 18:03:33 +0900

    feat : #232 survey api 개발

--- back/src/main/java/com/sail/back/report/controller/SurveyController.java
+++ back/src/main/java/com/sail/back/report/controller/SurveyController.java
@@ -1,30 +1,37 @@
 package com.sail.back.report.controller;
 
 import com.sail.back.global.utils.MessageUtils;
+import com.sail.back.report.model.dto.request.SaveSurveyRequest;
 import com.sail.back.report.model.entity.enums.SurveyType;
+import com.sail.back.report.model.service.SurveyService;
 import com.sail.back.user.model.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/v1/survey")
 public class SurveyController {
-
-
-    @PostMapping("/v1/report/first/{surveyType}")
+    private final SurveyService surveyService;
+    @PostMapping("/save/{consultingId}")
     public ResponseEntity<MessageUtils> addSurvey(
             @AuthenticationPrincipal User user,
-            @PathVariable SurveyType surveyType
-    ){
-
+            @PathVariable Long consultingId,
+            @RequestBody SaveSurveyRequest request
+            ){
+        surveyService.saveSurvey(user,request,consultingId);
         return ResponseEntity.ok().body(MessageUtils.success());
+    }
+
+    @GetMapping("/find/{consultingId}")
+    public ResponseEntity<MessageUtils> findReport(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long consultingId
+    ){
+        return
     }
 }
