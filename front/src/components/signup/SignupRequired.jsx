481ba68faddc90ef4d6ec6a3fda1aaf46ce27b8e
Joonsun <joonsun1006@naver.com>
2024-02-09 00:51:23 +0900

    feat: signup required axios work

--- front/src/components/signup/SignupRequired.jsx
+++ front/src/components/signup/SignupRequired.jsx
@@ -9,45 +9,90 @@ import CenterContainer from "../common/CenterContainer";
 import Button from "../common/Button";
 
 import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SignupRequired = () => {
-  // 여기서 필수정보를 axios로 쏜다.
-  // 이렇게 하면 null값인 필수정보들이 DB에 저장된다.
-  const response = axios.post('http://i10c106.p.ssafy.io:8080/v1/users/update', {
-    name: '',
-    nick_name: '',
-    gender: '',
-    birthdate_year: '',
-    birthdate_month: '',
-  });
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [gender, setGender] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // 서버로 전송할 데이터 객체 생성
+    const userData = {
+      name: name,
+      nick_name: nickname,
+      gender: gender,
+      birthdate_year: birthdate.slice(0, 4),
+      birthdate_month: birthdate.slice(4, 6),
+    };
+    try {
+      // 서버로 데이터 전송
+      // post? 여기서 토큰도 함께 보내줘야할 것 같다.
+      const response = await axios.post(
+        "http://i10c106.p.ssafy.io:8080/v1/users/update",
+        userData
+      );
+      console.log(response.data);
+      alert("회원가입 완료!");
+      navigate("/");
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
+  };
 
   return (
     <Card>
       <SignupRequiredHeader />
-      <form action="#">
+      <form onSubmit={handleSubmit}>
         <LabelStyle htmlFor="name">* 이름</LabelStyle>
-        <Input id="name" placeholder="이름 입력" required />
+        <Input
+          id="name"
+          placeholder="이름 입력"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
 
         <LabelStyle htmlFor="nickname">* 닉네임</LabelStyle>
-        <Input id="nickname" placeholder="닉네임 입력" required />
-        
+        <Input
+          id="nickname"
+          placeholder="닉네임 입력"
+          onChange={(e) => setNickname(e.target.value)}
+          required
+        />
+
         <LabelStyle htmlFor="birth">* 생년월일</LabelStyle>
-        <Input id="birth" placeholder="8자리 Ex) 19990118" required />
-        
-        <LabelStyle htmlFor="gender">* 성별</LabelStyle>   
+        <Input
+          id="birth"
+          placeholder="8자리 Ex) 19990118"
+          onChange={(e) => setBirthdate(e.target.value)}
+          required
+        />
+
+        <LabelStyle htmlFor="gender">* 성별</LabelStyle>
         <LabelContainer>
-          <LabelSignup htmlFor="male" >
+          <LabelSignup htmlFor="male">
             <Input
-              id="male" name="gender"
-              type="radio" width="1"
+              id="male"
+              name="gender"
+              type="radio"
+              width="1"
+              onChange={() => setGender("여성")}
               required
             />
             <RadioLabelText>여성</RadioLabelText>
           </LabelSignup>
           <LabelSignup htmlFor="female">
             <Input
-              width="1" id="female" name="gender"
+              width="1"
+              id="female"
+              name="gender"
               type="radio"
+              onChange={() => setGender("남성")}
             />
             <RadioLabelText>남성</RadioLabelText>
           </LabelSignup>
@@ -60,4 +105,4 @@ const SignupRequired = () => {
   );
 };
 
-export default SignupRequired
+export default SignupRequired;
