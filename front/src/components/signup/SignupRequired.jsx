e0e77680f0f6091c42a89d7e5ac41198b4e09ef2
Joonsun <joonsun1006@naver.com>
2024-02-10 00:52:28 +0900

    feat: #501 signup axios login try

--- front/src/components/signup/SignupRequired.jsx
+++ front/src/components/signup/SignupRequired.jsx
@@ -11,6 +11,7 @@ import Button from "../common/Button";
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const SignupRequired = () => {
   const [name, setName] = useState("");
@@ -19,7 +20,8 @@ const SignupRequired = () => {
   const [gender, setGender] = useState("");
 
   const navigate = useNavigate();
-
+  const accessToken = useSelector((state) => state.auth.logonUser.access_token);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
   const handleSubmit = async (e) => {
     e.preventDefault();
     // 서버로 전송할 데이터 객체 생성
@@ -30,12 +32,21 @@ const SignupRequired = () => {
       birthdate_year: birthdate.slice(0, 4),
       birthdate_month: birthdate.slice(4, 6),
     };
+    const config = {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    };
+    console.log("accessToken : ", accessToken);
+    console.log("isAuthenticated : ", isAuthenticated);
     try {
       // 서버로 데이터 전송
       // post? 여기서 토큰도 함께 보내줘야할 것 같다.
       const response = await axios.post(
         "http://i10c106.p.ssafy.io:8080/v1/users/update",
-        userData
+        userData,
+        config
       );
       console.log(response.data);
       alert("회원가입 완료!");
