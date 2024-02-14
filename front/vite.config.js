419d7090a0296fb09d9e3635e6e5f4cda19e7a56
김형민 <kimtuti@naver.com>
2024-02-14 16:35:59 +0900

    fix: vite 웹소켓 충돌 해결

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    hmr: {
+      protocol: 'ws', // WebSocket 프로토콜 설정 (일반적으로 'ws' 또는 'wss')
+      host: 'localhost', // WebSocket 호스트 설정
+      port: 12345 // WebSocket 포트 설정
+    }
+  }
 })
