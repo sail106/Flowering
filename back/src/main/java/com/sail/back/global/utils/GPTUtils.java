e54c194429cb44fc5413c01aa2f6332fdcc537f7
김형민 <kimtuti@naver.com>
2024-02-15 03:16:10 +0900

    feat : gpt api init

---
+++ back/src/main/java/com/sail/back/global/utils/GPTUtils.java
@@ -0,0 +1,15 @@
+package com.sail.back.global.utils;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class GPTUtils {
+
+
+
+}
