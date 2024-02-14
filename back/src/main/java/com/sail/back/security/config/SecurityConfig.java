962985f6a5355a6f3d5c055d2b2b3f635d4b5f8e
leikra44 <leikra44@gmail.com>
2024-02-15 01:04:17 +0900

    product URI add

--- back/src/main/java/com/sail/back/security/config/SecurityConfig.java
+++ back/src/main/java/com/sail/back/security/config/SecurityConfig.java
@@ -24,8 +24,7 @@ import java.util.Collections;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig{
-
-    private static final String[] ALLOWED_URIS = {"/v1/users/regist","/v1/auth/**","/v1/faq/**","/v1/email/**","/v1/contents/**","/verifyIamport/**"};
+    private static final String[] ALLOWED_URIS = {"/v1/users/regist","/v1/auth/**","/v1/faq/**","/v1/email/**","/v1/contents/**","/verifyIamport/**","v1/product/**"};
     private final JwtFilter jwtFilter;
     private final CustomOAuth2Service customOAuth2Service;
     private final OAuthSuccessHandler oAuth2SuccessHandler;
