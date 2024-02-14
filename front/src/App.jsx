261e2fb626287996e6c89eb77b426cd2845e0592
전원빈 <wjssoqls@gachon.ac.kr>
2024-02-14 19:48:45 +0900

    fix:App FinalResultInput 주석

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,124 +1,124 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
 // import LoginForm from "./components/LoginForm";
-import PasswordFindForm from "./components/PasswordFindForm";
-import SpinnerTest from "./components/SpinnerTest";
-import SignupForm from "./components/signup/SignupForm";
-import SignupPw from "./components/signup/SignupPw";
-import SignupRequired from "./components/signup/SignupRequired";
-import { Page } from "./components/common/Page";
-import MyPage from "./components/MyPage";
-import Navbar from "./components/Navbar";
-import GlobalStyles from "../GlobalStyles";
-import MainPage from "./components/MainPage";
-import CommunityReservation from "./components/community/CommunityReservation";
-import FaQ from "./components/FaQ";
-import Footer from "./components/Footer";
-import EditMyInfo from "./components/EidtMyInfo";
+import PasswordFindForm from './components/PasswordFindForm';
+import SpinnerTest from './components/SpinnerTest';
+import SignupForm from './components/signup/SignupForm';
+import SignupPw from './components/signup/SignupPw';
+import SignupRequired from './components/signup/SignupRequired';
+import { Page } from './components/common/Page';
+import MyPage from './components/MyPage';
+import Navbar from './components/Navbar';
+import GlobalStyles from '../GlobalStyles';
+import MainPage from './components/MainPage';
+import CommunityReservation from './components/community/CommunityReservation';
+import FaQ from './components/FaQ';
+import Footer from './components/Footer';
+import EditMyInfo from './components/EidtMyInfo';
 
-import OneToManyVideoChat from "./components/community/OneToManyVideoChat";
-import OneToOneVideoChat from "./components/OneToOneVideoChat";
-import CommunityModify from "./components/modify/CommunityModify";
-import ExpertsIntroduction from "./components/experts/ExpertsIntroduction";
-import ExpertsProfileBibi from "./components/experts/ExpertsProfileBibi";
-import CommunityPage from "./components/CommunityPage";
-import ExpertPage from "./components/ExpertPage";
-import BeautyConsulting from "./components/BeuatyConsulting";
-import ExpertConsulting from "./components/mypage/ExpertConsulting";
-import ExpertsProfileRegistration from "./components/experts/ExpertsProfileRegistration";
-import ExpertsReservation from "./components/experts/ExpertsReservation";
-import Review from "./components/experts/Review";
-import ExpertsProfileLeina from "./components/experts/ExpertsProfileLeina";
-import ExpertsProfileDiana from "./components/experts/ExpertsProfileDiana";
-import ExpertsProfileRuna from "./components/experts/ExpertsProfileRuna";
-import Manager from "./components/ManagerPage/Manager";
-import LoginForm from "./components/LoginForm";
+import OneToManyVideoChat from './components/community/OneToManyVideoChat';
+import OneToOneVideoChat from './components/OneToOneVideoChat';
+import CommunityModify from './components/modify/CommunityModify';
+import ExpertsIntroduction from './components/experts/ExpertsIntroduction';
+import ExpertsProfileBibi from './components/experts/ExpertsProfileBibi';
+import CommunityPage from './components/CommunityPage';
+import ExpertPage from './components/ExpertPage';
+import BeautyConsulting from './components/BeuatyConsulting';
+import ExpertConsulting from './components/mypage/ExpertConsulting';
+import ExpertsProfileRegistration from './components/experts/ExpertsProfileRegistration';
+import ExpertsReservation from './components/experts/ExpertsReservation';
+import Review from './components/experts/Review';
+import ExpertsProfileLeina from './components/experts/ExpertsProfileLeina';
+import ExpertsProfileDiana from './components/experts/ExpertsProfileDiana';
+import ExpertsProfileRuna from './components/experts/ExpertsProfileRuna';
+import Manager from './components/ManagerPage/Manager';
+import LoginForm from './components/LoginForm';
 
-import FirstSurveyEnd from "./components/FirstSurveyEnd";
+import FirstSurveyEnd from './components/FirstSurveyEnd';
 
-import PhotoTest from "./components/PhotoTest";
+import PhotoTest from './components/PhotoTest';
 
-import FinalSurveyResult from "./components/FinalSurveyResult";
-import Orderpage from "./components/OrderPage";
-import OrderResult from "./components/OrderResult";
-import ExpertsProfileCommon from "./components/experts/ExpertsProfilecommon";
+import FinalSurveyResult from './components/FinalSurveyResult';
+import Orderpage from './components/OrderPage';
+import OrderResult from './components/OrderResult';
+import ExpertsProfileCommon from './components/experts/ExpertsProfilecommon';
 
-import SecondSurveyResult from "./components/SecondSurveyResult";
-import FirstSurvey from "./components/FirstSurvey";
-import FirstSurveyResult from "./components/FirstSurveyResult";
-import FinalresultInput from "./components/finalResultForm/FinalResultInput";
+import SecondSurveyResult from './components/SecondSurveyResult';
+import FirstSurvey from './components/FirstSurvey';
+import FirstSurveyResult from './components/FirstSurveyResult';
+// import FinalresultInput from "./components/finalResultForm/FinalResultInput";
 
-import SecondSurveyEnd from "./components/SecondSurveyEnd";
+import SecondSurveyEnd from './components/SecondSurveyEnd';
 
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID,
+	apiKey: import.meta.env.VITE_APP_API_KEY,
+	authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_APP_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_APP_APP_ID,
 };
 initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <Page>
-        <Navbar />
-        <Routes>
-          <Route path="/OneToManyVideoChat" element={<OneToManyVideoChat />} />
-          <Route path="/OneToOneVideoChat" element={<OneToOneVideoChat />} />
-          {/* <Route path="/admin" element={<Admin />} /> */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/pw" element={<PasswordFindForm />} />
-          <Route path="/test" element={<SpinnerTest />} />
-          <Route path="/signup" element={<SignupForm />} />
-          <Route path="/signupPw" element={<SignupPw />} />
-          <Route path="/signupRequired" element={<SignupRequired />} />
-          <Route path="/communityReservation" element={<CommunityReservation />} />
-          <Route path="/orderpage" element={<Orderpage />} />
-          <Route path="/orderResult" element={<OrderResult />} />
-          <Route path="/mypage/:routeid" element={<MyPage />} />
-          <Route path="/expertmypage/:routeid" element={<ExpertPage />} />
-          <Route path="/editmyinfo/:routeid" element={<EditMyInfo />} />
-          <Route path="/faq" element={<FaQ />} />
-          <Route path="/beautyconsulting" element={<BeautyConsulting />} />
-          <Route path="/communitymod" element={<CommunityModify />} />
-          <Route path="/experts" element={<ExpertsIntroduction />} />
-          <Route path="/expertsProfilecommon/:id" element={<ExpertsProfileCommon />} />
+	return (
+		<>
+			<GlobalStyles />
+			<Page>
+				<Navbar />
+				<Routes>
+					<Route path='/OneToManyVideoChat' element={<OneToManyVideoChat />} />
+					<Route path='/OneToOneVideoChat' element={<OneToOneVideoChat />} />
+					{/* <Route path="/admin" element={<Admin />} /> */}
+					<Route path='/' element={<MainPage />} />
+					<Route path='/login' element={<LoginForm />} />
+					<Route path='/pw' element={<PasswordFindForm />} />
+					<Route path='/test' element={<SpinnerTest />} />
+					<Route path='/signup' element={<SignupForm />} />
+					<Route path='/signupPw' element={<SignupPw />} />
+					<Route path='/signupRequired' element={<SignupRequired />} />
+					<Route path='/communityReservation' element={<CommunityReservation />} />
+					<Route path='/orderpage' element={<Orderpage />} />
+					<Route path='/orderResult' element={<OrderResult />} />
+					<Route path='/mypage/:routeid' element={<MyPage />} />
+					<Route path='/expertmypage/:routeid' element={<ExpertPage />} />
+					<Route path='/editmyinfo/:routeid' element={<EditMyInfo />} />
+					<Route path='/faq' element={<FaQ />} />
+					<Route path='/beautyconsulting' element={<BeautyConsulting />} />
+					<Route path='/communitymod' element={<CommunityModify />} />
+					<Route path='/experts' element={<ExpertsIntroduction />} />
+					<Route path='/expertsProfilecommon/:id' element={<ExpertsProfileCommon />} />
 
-          <Route path="/expertsprofileBibi" element={<ExpertsProfileBibi />} />
-          <Route path="/expertsprofileLeina" element={<ExpertsProfileLeina />} />
-          <Route path="/expertsprofileDiana" element={<ExpertsProfileDiana />} />
-          <Route path="/expertsprofileRuna" element={<ExpertsProfileRuna />} />
-          <Route path="/expertconsulting" element={<ExpertConsulting />} />
-          <Route path="/communityHome" element={<CommunityPage />} />
-          <Route path="/expertsReservation" element={<ExpertsReservation />} />
-          <Route path="/review/:routeid" element={<Review />} />
+					<Route path='/expertsprofileBibi' element={<ExpertsProfileBibi />} />
+					<Route path='/expertsprofileLeina' element={<ExpertsProfileLeina />} />
+					<Route path='/expertsprofileDiana' element={<ExpertsProfileDiana />} />
+					<Route path='/expertsprofileRuna' element={<ExpertsProfileRuna />} />
+					<Route path='/expertconsulting' element={<ExpertConsulting />} />
+					<Route path='/communityHome' element={<CommunityPage />} />
+					<Route path='/expertsReservation' element={<ExpertsReservation />} />
+					<Route path='/review/:routeid' element={<Review />} />
 
-          <Route path="/expertsprofileregistration" element={<ExpertsProfileRegistration />} />
-          <Route path="/manager" element={<Manager />} />
+					<Route path='/expertsprofileregistration' element={<ExpertsProfileRegistration />} />
+					<Route path='/manager' element={<Manager />} />
 
-          <Route path="/finalresultinput" element={<FinalresultInput />} />
+					<Route path='/finalresultinput' element={<FinalresultInput />} />
 
-          <Route path="/phototest" element={<PhotoTest />} />
+					<Route path='/phototest' element={<PhotoTest />} />
 
-          <Route path="/secondsurveyresult" element={<SecondSurveyResult />}></Route>
-          <Route path="/firstsurvey" element={<FirstSurvey />} />
-          <Route path="/firstsurveyresult" element={<FirstSurveyResult />} />
-          <Route path="/firstsurveyend" element={<FirstSurveyEnd />} />
-          <Route path="/secondsurveyend" element={<SecondSurveyEnd />} />
-          <Route path="/finalresult" element={<FinalSurveyResult />} />
-        </Routes>
+					<Route path='/secondsurveyresult' element={<SecondSurveyResult />}></Route>
+					<Route path='/firstsurvey' element={<FirstSurvey />} />
+					<Route path='/firstsurveyresult' element={<FirstSurveyResult />} />
+					<Route path='/firstsurveyend' element={<FirstSurveyEnd />} />
+					<Route path='/secondsurveyend' element={<SecondSurveyEnd />} />
+					<Route path='/finalresult' element={<FinalSurveyResult />} />
+				</Routes>
 
-        <Footer />
-      </Page>
-    </>
-  );
+				<Footer />
+			</Page>
+		</>
+	);
 }
 
 export default App;
--- front/src/components/experts/ExpertsIntroduction.jsx
+++ front/src/components/experts/ExpertsIntroduction.jsx
@@ -1,151 +1,90 @@
-import Title from "../modify/Title";
-import styled from "styled-components";
-import Experts from "../common/Experts";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { setExpertList } from "../../store/ExpertsListSlice";
-import BIBI from "../../assets/BIBI.png";
-import LEINA from "../../assets/LEINA.png";
-import DIANA from "../../assets/DIANA.png";
-import RUNA from "../../assets/RUNA.png";
+import Title from '../modify/Title';
+import styled from 'styled-components';
+import Experts from '../common/Experts';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { setExpertList } from '../../store/ExpertsListSlice';
 
 const ExpertCard = styled.div`
-  justify-content: center;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
+	justify-content: center;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
 `;
 
 const Margin = styled.div`
-  margin-bottom: 130px;
+	margin-bottom: 130px;
 `;
 // 컴포넌트 정의
 const ExpertsIntroduction = () => {
-  const { access_token } = useSelector((state) => state.auth.logonUser);
-  const [expertsData, setExpertsData] = useState([]);
-  const [extraData, setextraData] = useState([]);
-  const dispatch = useDispatch();
+	const { access_token } = useSelector((state) => state.auth.logonUser);
+	const [expertsData, setExpertsData] = useState([]);
+	const [extraData, setextraData] = useState([]);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    console.log("ssss");
-    const fetchData = async () => {
-      try {
-        const token = access_token; // 여기에 액세스 토큰을 설정합니다.
-        const config = {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        };
+	useEffect(() => {
+		console.log('ssss');
+		const fetchData = async () => {
+			try {
+				const token = access_token; // 여기에 액세스 토큰을 설정합니다.
+				const config = {
+					headers: {
+						Authorization: `Bearer ${token}`,
+						'Content-Type': 'application/json',
+					},
+				};
 
-        const baseurl = import.meta.env.VITE_APP_BASE_URL;
+				const baseurl = import.meta.env.VITE_APP_BASE_URL;
+				const response = await axios.get(baseurl + 'consultant/list', config);
 
-        const response = await axios.get(baseurl + "consultant/list", config);
+				// 요청 성공 시 수행할 작업
+				console.log('data_body  :', response.data.data_body);
 
-        // 요청 성공 시 수행할 작업
-        console.log("Response:", response.data);
-        console.log("data_body  :", response.data.data_body);
+				setExpertsData(response.data.data_body); // response.data를 expertsData에 저장
+				dispatch(setExpertList(response.data.data_body));
+			} catch (error) {
+				console.error('Error :', error);
+				// alert('결제 실패');
+			}
+		};
 
-        console.log("hashhhh:", response.data.data_body.hash_tag_responses);
-        console.log("user_response:", response.data.data_body.user_response);
+		fetchData(); // fetchData 함수 호출
+	}, [access_token]);
 
-        setExpertsData(response.data.data_body); // response.data를 expertsData에 저장
-        dispatch(setExpertList(response.data.data_body));
-      } catch (error) {
-        console.error("Error :", error);
-        // alert('결제 실패');
-      }
-    };
+	if (expertsData.length > 0) {
+		console.log('succcc' + JSON.stringify(expertsData[1]));
 
-    fetchData(); // fetchData 함수 호출
-  }, [access_token]);
+		if (expertsData[1]?.hash_tag_responses.length ?? '' > 0) {
+			console.log('succcc', expertsData[1].hash_tag_responses[0].workplace);
+		}
+	}
 
-  if (expertsData.length > 0) {
-    console.log("succcc" + JSON.stringify(expertsData[1]));
-    // console.log('succcc', JSON.stringify(expertsData[1].user_response));
-    // console.log('succcc', JSON.stringify(expertsData[1].hash_tag_responses));
+	return (
+		<>
+			<Title text={'Beauty consulting experts'} />
 
-    if (expertsData[1]?.hash_tag_responses.length ?? "" > 0) {
-      console.log("succcc", expertsData[1].hash_tag_responses[0].workplace);
-    }
-  }
-
-  return (
-    <>
-      <Title text={"Beauty consulting experts"} />
-
-      <ExpertCard>
-        {expertsData.map((expert) => (
-          <Experts
-            key={expert.consultant_id} // 각 전문가의 ID를 키로 사용
-            id={expert.consultant_id} // 고유한 키 추가
-            nickname={expert.user_response.nickname}
-            text={expert.simple_introduce}
-            rate={expert.star}
-            ratenum={expert.reviewnum}
-            tag1={expert.hash_tag_responses.length > 0 ? expert?.hash_tag_responses[0]?.workplace ?? "" : ""}
-            tag2={expert.hash_tag_responses.length > 0 ? expert?.hash_tag_responses[1]?.workplace ?? "" : ""}
-            imgsrc={expert.user_response.profile_img_url}
-            width={"280px"}
-            height={"405px"}
-            path={"/expertsProfilecommon/" + expert.consultant_id}
-          />
-        ))}
-
-        <Experts
-          nickname={"BIBI"}
-          text={"당신만의 고유한 아름다움을 찾아드리겠습니다."}
-          rate={4.9}
-          ratenum={172}
-          tag1={"기초케어"}
-          tag2={"자연주의"}
-          imgsrc={BIBI}
-          width={"280px"}
-          height={"405px"}
-          path={"/expertsProfileBibi"}
-        />
-
-        <Experts
-          nickname={"LEINA"}
-          text={"나를 마음껏 표현할 수 있을 때, 진정 아름다워집니다."}
-          rate={4.7}
-          ratenum={289}
-          tag1={"스킨케어"}
-          tag2={"헤어스타일링"}
-          imgsrc={LEINA}
-          width={"305px"}
-          height={"370px"}
-          path={"/expertsProfileLeina"}
-        />
-        <Experts
-          nickname={"DIANA"}
-          text={"나의 강점을 살리며 다양하게 스타일링을 도전해보세요."}
-          rate={4.9}
-          ratenum={117}
-          tag1={"트렌드"}
-          tag2={"웨딩뷰티"}
-          imgsrc={DIANA}
-          width={"296px"}
-          height={"415px"}
-          path={"/expertsProfileDiana"}
-        />
-        <Experts
-          nickname={"RUNA"}
-          text={"건강하고 자연스러운 아름다움을 선물해드립니다."}
-          rate={4.8}
-          ratenum={390}
-          tag1={"에코뷰티"}
-          tag2={"컬러리스트"}
-          imgsrc={RUNA}
-          width={"294px"}
-          height={"363px"}
-          path={"/expertsProfileRuna"}
-        />
-      </ExpertCard>
-      <Margin />
-    </>
-  );
+			<ExpertCard>
+				{expertsData.map((expert) => (
+					<Experts
+						key={expert.consultant_id} // 각 전문가의 ID를 키로 사용
+						id={expert.consultant_id} // 고유한 키 추가
+						nickname={expert.user_response.nickname}
+						text={expert.simple_introduce}
+						rate={expert.star}
+						ratenum={expert.reviewnum}
+						tag1={expert.hash_tag_responses.length > 0 ? expert?.hash_tag_responses[0]?.workplace ?? '' : ''}
+						tag2={expert.hash_tag_responses.length > 0 ? expert?.hash_tag_responses[1]?.workplace ?? '' : ''}
+						imgsrc={expert.user_response.profile_img_url}
+						width={'280px'}
+						height={'405px'}
+						path={'/expertsProfilecommon/' + expert.consultant_id}
+					/>
+				))}
+			</ExpertCard>
+			<Margin />
+		</>
+	);
 };
 
 export default ExpertsIntroduction;
