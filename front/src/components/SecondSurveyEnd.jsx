1d1feca8cc7aec649fa553bac9350ac182cd5276
정준원 <junwon1131@naver.com>
2024-02-16 10:29:49 +0900

    secondsurveyend 끝난후 00과 00 에 만나요 메시지 동적으로 수정

--- front/src/components/SecondSurveyEnd.jsx
+++ front/src/components/SecondSurveyEnd.jsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 import { ButtonBox } from '../components/common/Button';
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Consulting1stepresultpage = styled.div`
 	margin-top: -180px;
@@ -53,6 +57,47 @@ const StepImg = styled.img`
 `;
 
 const Finish2step = () => {
+	const location = useLocation();
+	const consultingId = location.state.value.consultingId;
+	const User = useSelector(
+		(state) => state.auth.logonUser
+	);
+	const baseurl = import.meta.env.VITE_APP_BASE_URL;
+
+	const { name, role, id, nickname, imageUrl, access_token, email } =
+		useSelector((state) => state.auth.logonUser);
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		console.log('innn');
+
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(
+					`${baseurl}consultings/${consultingId}`,
+					{
+						headers: {
+							Authorization: `Bearer ${access_token}`,
+						},
+					}
+				);
+				console.log(response.data.data_body);
+				setData(response.data.data_body);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+		fetchData(); // Initial fetch
+	}, [access_token]); // Re-fetch when Token.access_token changes
+
+
+	useEffect(() => {
+
+		console.log(data);
+
+	}, [data]); // Re-fetch when Token.access_token changes
+
+
 	return (
 		<Consulting1stepresultpage>
 			<Margin />
@@ -60,10 +105,13 @@ const Finish2step = () => {
 			<CheckImg src='src/assets/checkcircle.png' alt='1차 설문 완료' />
 			<H3>컨설팅을 위한 모든 테스트가 완료되었습니다.</H3>
 			<P>
-				키티공주님,
+				{User.name}님,
 				<br />
-				2024.01.18(목) 14:00에 <br />
-				BIBI님과 만나요!
+				{/* 2024.01.18(목) 14:00에 <br /> */}
+				{data?.date ?? ''} {data?.time ?? ''} 에 <br />
+
+				{data?.consultant_data
+					?.user_response.name ?? ''} 님과 만나요!
 			</P>
 			<P>
 				마이페이지 - 마이컨설팅 - 바로가기를 클릭하시면 <br />
